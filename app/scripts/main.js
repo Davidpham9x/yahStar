9192260b1f6adeb0485f3f5b9e8c5d22696970f1
Minh Huy Pham <minhhuy.pham@ogilvy.com>
2016-11-30 18:02:18 +0700

    Add popup video

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -30,6 +30,7 @@ var yahStar = window.yahStar || {}; //global namespace for YOUR yahStar, Please 
         init: function() { //initialization code goes here
             $.support.cors = true;
 
+            this.initFormElements();
             this.initAutocomplete();
             this.initShowMenuMobile();
             this.initShowSearchMobile();
@@ -38,12 +39,63 @@ var yahStar = window.yahStar || {}; //global namespace for YOUR yahStar, Please 
                 yahStar.Global.initModalCreateEvent();
             }
 
+            if ( $('.page--video').length ) {
+                yahStar.Global.initModalYoutube();
+            }
+
             $(document.body).click(function(e) {
                 if ( $(e.target).hasClass('txt-search') ) {
                     return;
                 } else {
                     $('.autocomplete').hide();
                 }
+            });
+        },
+
+        initFormElements: function() {
+            $('.radio-wrapper .input-radio').each(function() {
+                if ($(this).is(':checked')) {
+                    $('.input-radio[name="' + $(this).attr('name') + '"]').parents('.radio-selected').removeClass('radio-selected');
+                    $(this).parents('.radio-wrapper').addClass('radio-selected');
+                }
+            });
+
+            $(document).on('change', '.radio-wrapper .input-radio', function() {
+                $('input[name="' + $(this).attr('name') + '"]').each(function() {
+                    if ($(this).not(':checked')) {
+                        $(this).parent().removeClass('radio-selected');
+                    }
+                });
+
+                if ($(this).is(':checked')) {
+                    $(this).parents('.radio-wrapper').addClass('radio-selected');
+                }
+            });
+
+            //Checkbox Wrapper
+            $('.checkbox-wrapper .input-checkbox').each(function() {
+                if ($(this).is(':checked')) {
+                    $(this).parents('.checkbox-wrapper').addClass('checked');
+                }
+            });
+
+            $(document).on('click', '.checkbox-wrapper .input-checkbox', function() {
+                if ($(this).is(':checked')) {
+                    $(this).parents('.checkbox-wrapper').addClass('checked');
+                } else if ($(this).not(':checked')) {
+                    $(this).parents('.checkbox-wrapper').removeClass('checked');
+                }
+            });
+
+            //Select Wrapper
+            $('.select-wrapper').each(function() {
+                if ($(this).find('span').length <= 0) {
+                    $(this).prepend('<span>' + $(this).find('select option:selected').text() + '</span>');
+                }
+            });
+
+            $(document).on('change', '.select-wrapper select', function() {
+                $(this).prev('span').replaceWith('<span>' + $(this).find('option:selected').text() + '</span>');
             });
         },
 
@@ -87,6 +139,36 @@ var yahStar = window.yahStar || {}; //global namespace for YOUR yahStar, Please 
             });
 
             $('#modal--create-event').find('.close').off('click').on('click', function(e) {
+                e.preventDefault();
+
+                var magnificPopup = $.magnificPopup.instance;
+                magnificPopup.close(); // Close popup that is currently opened
+            });
+        },
+
+        initModalYoutube: function() {
+            $.magnificPopup.open({
+                items: {
+                    src: '#modal--youtube'
+                },
+                type: 'inline',
+                removalDelay: 500, // Delay removal by X to allow out-animation
+                callbacks: {
+                    beforeOpen: function() {
+                        this.st.mainClass = 'mfp-zoom-out';
+                    },
+                    open: function () {
+                        new Foundation.Equalizer( $('#modal__row-youtube') ).applyHeight();
+
+                        $('#youtube-tabs').on('change.zf.tabs', function() {
+                            new Foundation.Equalizer( $('#modal__row-youtube') ).applyHeight();
+                        });
+                    }
+                },
+                midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+            });
+
+            $('#modal--youtube').find('.close').off('click').on('click', function(e) {
                 e.preventDefault();
 
                 var magnificPopup = $.magnificPopup.instance;
